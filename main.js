9b4f0cd566528cb3844883d13653f4aa5a9fc6d0
emeryss <emeryss.steam@gmail.com>
2024-07-10 02:10:42 +0900

    first take

---
+++ main.js
@@ -0,0 +1,14 @@
+const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc85`
+let news=[]
+const getLatestNews = async() => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response =  await fetch(url);
+    const data = await response.json();
+    console.log("rrrr", response);
+    console.log("dddd", data);
+    console.log("dddd", data.articles);
+    news = data.articles;
+    console.log("news", news);
+}
+
+getLatestNews(); 
