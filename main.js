aba0055e30936407e949741e0917eaa3ff8016bd
emeryss <emeryss.steam@gmail.com>
2024-07-10 02:31:22 +0900

    retry

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc85`
 let news=[]
 const getLatestNews = async() => {
-    const url = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+    const url = `https://noona-20240710.netlify.app/top-headlines?`
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     const response =  await fetch(url);
     const data = await response.json();
