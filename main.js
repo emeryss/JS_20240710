49abb2e4c0ba97c9ec8e475511f593e673b0d218
emeryss <emeryss.steam@gmail.com>
2024-07-10 02:14:31 +0900

    second take

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc85`
 let news=[]
 const getLatestNews = async() => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     const response =  await fetch(url);
     const data = await response.json();
     console.log("rrrr", response);
