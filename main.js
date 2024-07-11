e4f3b938bea7726c2b2a9987dce046d780b59412
emeryss <emeryss.steam@gmail.com>
2024-07-12 05:24:38 +0900

    7/11

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc85`
 let newsList=[]
+const menus = document.querySelectorAll(".menus button")
+console.log("mmm", menus)
+menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+
 const getLatestNews = async() => {
-    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    // const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
     const response =  await fetch(url);
     const data = await response.json();
     console.log("rrrr", response);
@@ -11,6 +15,28 @@ const getLatestNews = async() => {
     newsList = data.articles;
     render();
     console.log("news", newsList);
+}
+
+const getNewsByCategory = async (event)=>{
+    const category = event.target.textContent.toLowerCase();
+    console.log("category", category)
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Ddd", data);
+    newsList = data.articles;
+    render();
+}
+
+const getNewsByKeyword = async() =>{
+    const keyword = document.getElementById("seach-input").value;
+    console.log("keyword", keyword)
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("keyword data", data);
+    newsList = data.articles;
+    render();
 }
 
 const render =()=>{
@@ -36,3 +62,4 @@ const render =()=>{
 }
 
 getLatestNews(); 
+
