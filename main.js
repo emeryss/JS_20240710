b96582e9cd15e2de2c5cc691ea665290b5f23fa8
emeryss <emeryss.steam@gmail.com>
2024-07-13 00:25:23 +0900

    error handling

--- main.js
+++ main.js
@@ -1,44 +1,77 @@
 const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc85`
+// const API_KEY=`ac0ee77ab7cb4975a4485d0265c3fc8`   //error 유발용.
+
+
+//forDeploy; 0은 newsAPI, 1은 netlify 배표용.
+let url=new URL (`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+let forDeploy=1 
+
 let newsList=[]
+
+//main menu
 const menus = document.querySelectorAll(".menus button")
-console.log("mmm", menus)
 menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
 
+//side menu
+const sideMenu = document.querySelectorAll("#menu-list button")
+sideMenu.forEach(sideMenu=>sideMenu.addEventListener("click", (event)=>getNewsByCategory(event)))
+
+// let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`)
+// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+
+const getNews = async()=>{
+    try{
+        const response =  await fetch(url);
+        console.log("rrr", response)
+        const data = await response.json();
+        if(response.status===200){
+            if(data.articles.length === 0){
+                throw new Error("No results for this search");
+            }else{
+                newsList = data.articles;
+                render();
+            }
+            
+        }else{
+            throw new Error(data.message)
+        } 
+    }catch(error){
+        errorRender(error.message)
+    }
+    
+}
+
 const getLatestNews = async() => {
-    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-    const response =  await fetch(url);
-    const data = await response.json();
-    console.log("rrrr", response);
-    console.log("dddd", data);
-    console.log("dddd", data.articles);
-    newsList = data.articles;
-    render();
-    console.log("news", newsList);
+    if (forDeploy==0){
+        url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+    }else if(forDeploy=1){
+        url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`)
+    }
+
+    await getNews();
 }
 
 const getNewsByCategory = async (event)=>{
     const category = event.target.textContent.toLowerCase();
-    console.log("category", category)
-    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("Ddd", data);
-    newsList = data.articles;
-    render();
+    if (forDeploy==0){
+        url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+    }else if(forDeploy=1){
+        url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
+    }
+
+    await getNews();
 }
 
 const getNewsByKeyword = async() =>{
     const keyword = document.getElementById("seach-input").value;
-    console.log("keyword", keyword)
-    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("keyword data", data);
-    newsList = data.articles;
-    render();
+
+    if (forDeploy==0){
+        url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
+    }else if(forDeploy=1){
+        url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
+    }
+
+    await getNews();
 }
 
 const render =()=>{
@@ -69,6 +102,8 @@ const render =()=>{
     document.getElementById("news-board").innerHTML=newsHTML
 }
 
+
+
 const openNav = () =>{
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -84,6 +119,14 @@ const openSearchBox=()=>{
     }else{
         inputArea.style.display = "inline";
     }
+}
+
+const errorRender=(errorMessage)=>{
+    const errorHTML = `<div class="alert alert-danger" role="alert">
+        ${errorMessage}
+    </div>`;
+
+    document.getElementById("news-board").innerHTML = errorHTML
 }
 
 getLatestNews(); 
