471a8615453f54b5b645011d25ca12846d7b7cdd
emeryss <emeryss.steam@gmail.com>
2024-07-12 22:54:16 +0900

    3rd take

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ console.log("mmm", menus)
 menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
 
 const getLatestNews = async() => {
-    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`
+    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
     const response =  await fetch(url);
     const data = await response.json();
@@ -20,7 +20,8 @@ const getLatestNews = async() => {
 const getNewsByCategory = async (event)=>{
     const category = event.target.textContent.toLowerCase();
     console.log("category", category)
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
     const response = await fetch(url);
     const data = await response.json();
     console.log("Ddd", data);
@@ -31,7 +32,8 @@ const getNewsByCategory = async (event)=>{
 const getNewsByKeyword = async() =>{
     const keyword = document.getElementById("seach-input").value;
     console.log("keyword", keyword)
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
+    const url = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
     const response = await fetch(url);
     const data = await response.json();
     console.log("keyword data", data);
@@ -43,15 +45,21 @@ const render =()=>{
     const newsHTML = newsList.map(news=>`
         <div class="row news">
             <div class="col-lg-4">
-                <img class="news-image-size" src=${news.urlToImage} />
+                <img class="news-image-size" src= "${news.urlToImage || "./images/image_not_available.png"}"/>
             </div>
             <div class="col-lg-8">
                 <h2>${news.title}</h2>
                 <p>
-                    ${news.description}
+                    ${
+                        news.description == null || news.description == ""
+                        ? "내용 없음"
+                        : news.description.length >200
+                        ? news.description.substring(0,200) + "..."
+                        : news.description
+                    }
                 </p>
                 <div>
-                    ${news.source.name} * ${news.publishedAt}
+                    ${news.source.name || "no source"} * ${moment(news.publishedAt).fromNow()}
                 </div>
             </div>
         </div>
